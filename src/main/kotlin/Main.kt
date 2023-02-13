131ae31b1740b8082d9c86036d7cac86b04b96b1
MrKearns <jkearns631@students.mchenry.edu>
2023-02-13 14:51:38 -0600

    first

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,37 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+
+    // ----------- TODAY"S DATE & FORMATTING ----------
+
+    var today = LocalDate.now()
+    val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
+    var formattedToday = today.format(formatter)
+
+
+    // ---------- LAST FULL MOON ----------
+
+    val lastFullMoon: LocalDate = LocalDate.of(2023, 2, 5)
+
+    // Next full from last
+
+    var lastPlus30 = lastFullMoon.plusDays(30)
+
+    // calculate the number of days from today till next full moon
+
+    var daysToNextFullMoon = today.until(lastPlus30, ChronoUnit.DAYS)
+
+    // add DAYS TILL to TODAY
+
+    var nextFullDate = today.plusDays(daysToNextFullMoon).format(formatter)
+
+    // ---------- PRINT STATEMENTS ----------
+
+    println("Today is $formattedToday")
+
+    println("The next full moon is in: $daysToNextFullMoon ($nextFullDate)")
+
 }
